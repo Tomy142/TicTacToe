87cf42b59ae7f8827f1a2c9343c24756f7f3929a
Tomy142 <tomaslema.01@gmail.com>
2025-08-29 12:31:33 -0300

    UpdateCode

--- Raylib/tictactoe.c
+++ Raylib/tictactoe.c
@@ -6,6 +6,10 @@
 
 #define BOARD_SIZE 3
 #define CELL_SIZE 150
+#define LINES_COLOR (Color){35, 31, 32, 1}
+#define WINNING_LINE (Color){255, 215, 0, 1}
+#define X_COLOR (Color){229, 68, 109, 1}
+#define O_COLOR (Color){73, 220, 177, 1}
 
 typedef enum{
     MENU,
@@ -52,4 +56,66 @@ int CheckWinner(Game *game){
             return board[0][i];
         }
     }
+
+    if(board[0][0] != 0 && board[0][0] == board○[1][1] && board[0][0] == board[2][2]){
+        return board[0][0];
+    }
+
+    if(board[0][2] != 0 && board[0][2] = board[1][1] && board[0][2] == board[2][0]){
+        return board[0][2];
+    }
+
+    int isFull= 1;
+    for(int i = 0; i < BOARD_SIZE; i++){
+        for(int j = 0; j < BOARD_SIZE; j++){
+            if(board[i][j] == 0){
+                isFull = 0;
+                break;
+            }
+        }
+        if(!isFull)break;
+    }
+    if(isFull) return 3;
+
+    return 0;
+}
+
+void DrawBoard(Game *game){
+    int offsetX = (SCREEN_WIDTH - BOARD_SIZE * CELL_SIZE) / 2;
+    int offsetY = (SCREEN_WIDTH - BOARD_SIZE * CELL_SIZE) / 2;
+
+    for(int i = 1; i < BOARD_SIZE; i++){
+        DrawLine(offsetX + i * CELL_SIZE, offsetY,
+                offsetX + i * CELL_SIZE, offsetY + BOARD_SIZE * CELL_SIZE, LINES_COLOR);
+        
+        DrawLine(offsetX, offsetY + i * CELL_SIZE,
+                offsetX + BOARD_SIZE * CELL_SIZE, offsetY + i * CELL_SIZE, LINES_COLOR);
+    }
+
+    for(int i = 0; i < BOARD_SIZE; i++){
+        for(int j = 0; j < BOARD_SIZE; j++){
+            int posX = offsetX + j * CELL_SIZE + CELL_SIZE / 2;
+            int posY = offsetY + j * CELL_SIZE + CELL_SIZE / 2;
+
+            if(game->board[i][j] == 1){
+                DrawLine(posX - 40, posY - 40, posX + 40, posY + 40, X_COLOR);
+                DrawLine(posX + 40, posY - 40, posX - 40, posY + 40, X_COLOR);
+            }else if(game->board[i][j] == 2){
+                DrawCircleLines(posX, posY, 40, O_COLOR);
+            }
+        }
+    }
+
+    if(game->state == GAMEPLAY && game->mouseHoverX >= 0 && game->mouseHoverY >= 0){
+        int hoverX = offsetX + game->mouseHoverX * CELL_SIZE;
+        int hoverY = offsetY + game->mouseHoverY * CELL_SIZE;
+
+        DrawRectangleLines(hoverX, hoverY, CELL_SIZE, CELL_SIZE,
+                            (game->currentPlayer  == 1) ? Fade(X_COLOR, 0.5f) : Fade(O_COLOR, 0.5f));
+    }
+}
+
+void DrawMenu(Game *game){
+    DrawText("TaTeTi", SCREEN_WIDTH/2 - MeasureText("TaTeTi", 40)/2, 100, 40, DARKGRAY);
+    DrawText("Presiona ESPACIO para comenzar", SCREEN_WIDTH/2 - MesureText("Presiona ESPACIO para comenzar", 20)/2, 200, 20, GRAY);
 }
